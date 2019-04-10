e1061c66e0af7a6feff73cb421b2661274ccd706
Elton Silva <elton.chrls@gmail.com>
2019-04-09 21:15:58 -0300

    Envio de tags

--- www/app/controllers/iframe_controller.js
+++ www/app/controllers/iframe_controller.js
@@ -1,8 +1,12 @@
 var app = angular.module('codhab.controllers.iframe', []);
 app.controller('IframeController', function ($scope, $stateParams, $state) {
-  $scope.iframe_url = AppEnv.base_iframe_url + $stateParams.url
-  $scope.iframe_url = $scope.iframe_url.replace('_', '/')
+  $scope.device_id  = window.localStorage.getItem('device_id');
+  $scope.push_token = window.localStorage.getItem('push_token');
 
+  $scope.iframe_url = AppEnv.base_iframe_url + $stateParams.url 
+  $scope.iframe_url = $scope.iframe_url.replace('_', '/')
+  $scope.iframe_url = $scope.iframe_url + '?device_id=' + $scope.device_id + '&push_token=' + $scope.push_token
+  
   $scope.$on('$ionicView.enter', function () {
     if (!navigator.onLine) {
       $state.go("disconnect");
